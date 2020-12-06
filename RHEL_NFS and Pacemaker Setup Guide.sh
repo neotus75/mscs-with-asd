ff9a493e4cc39b8f7735a9cef4d19357fefa7087
neotus75 <patrick.shim@live.co.kr>
2020-12-06 17:21:27 +0900

    r51-december-06 / description and comments added

--- RHEL_NFS and Pacemaker Setup Guide.sh
+++ RHEL_NFS and Pacemaker Setup Guide.sh
@@ -63,34 +63,34 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.re
 sudo yum install azure-cli -y
 
 az login
-az ad sp create-for-rbac -n "pcmk-nfs-cluster" --role owner --scopes /subscriptions/<subId>/resourceGroups/<Resource Group> # your sub ID and resource group
+az ad sp create-for-rbac -n "pcmk-nfs-cluster" --role owner --scopes /subscriptions/a370ff12-d748-4091-8749-a21c085d368f/resourceGroups/nfs-pcmk-asd-resources # your sub ID and resource group
 
 ### this assumes you have azure cli on your linux box.   
 {
-  "appId": "<appId>",
+  "appId": "7e35bb0b-1fe4-4b5d-b759-5198065ac0d1",
   "displayName": "pcmk-nfs-cluster",
   "name": "http://pcmk-nfs-cluster",
-  "password": "<pass>",
-  "tenant": "<tenantId>"
+  "password": "YilNOOqMDmEVbR.n8YUijg2XGHh_9gCsIO",
+  "tenant": "72f988bf-86f1-41af-91ab-2d7cd011db47"
 }
 
 ### copy and paste your app information and replace the value below to create cluster fencing
-fence_azure_arm -l <appId> -p <passwd> --resourceGroup nfs-pcmk-asd-resources --tenantId <tenantId> --subscriptionId <subscriptionId> -o list
-pcs stonith create nfs_pcmk_stonith fence_azure_arm login=<appId> passwd=<passwd> resourceGroup=nfs-pcmk-asd-resources tenantId=<tenantId> subscriptionId=<subId> pcmk_reboot_retries=3
+fence_azure_arm -l 7e35bb0b-1fe4-4b5d-b759-5198065ac0d1 -p YilNOOqMDmEVbR.n8YUijg2XGHh_9gCsIO --resourceGroup nfs-pcmk-asd-resources --tenantId 72f988bf-86f1-41af-91ab-2d7cd011db47 --subscriptionId a370ff12-d748-4091-8749-a21c085d368f -o list
+pcs stonith create nfs_pcmk_stonith fence_azure_arm login=7e35bb0b-1fe4-4b5d-b759-5198065ac0d1 passwd=YilNOOqMDmEVbR.n8YUijg2XGHh_9gCsIO resourceGroup=nfs-pcmk-asd-resources tenantId=72f988bf-86f1-41af-91ab-2d7cd011db47 subscriptionId=a370ff12-d748-4091-8749-a21c085d368f pcmk_reboot_retries=3
 
 ### create partition on Azure Shared Disk (/dev/sdc) 
 fdisk /dev/sdc # n -> p -> w -> default sizes
 
 ### create volume group, logical volume, and format it to ext4
-pvcreate /dev/sdc1
-vgcreate pcmkvg /dev/sdc1
-lvcreate -l 100%FREE -n pcmklv pcmkvg 
-lvs
-mkfs.ext4 /dev/pcmkvg/pcmklv
+pvcreate /dev/sdc1                      # creates a physical volume 
+vgcreate pcmkvg /dev/sdc1               # creates a volume group
+lvcreate -l 100%FREE -n pcmklv pcmkvg   # creates logical volume of 100% in size, named "n", in the volume group called pcmkvg 
+lvs                                     # reports information about the logical volume
+mkfs.ext4 /dev/pcmkvg/pcmklv            # creates filesystem (ext4) out of /dev/pcmkvg/pcmklv
 
 ### prep to create share directory for NFS service
-mkdir /nfsshare
-mount /dev/pcmkvg/pcmklv /nfsshare
+mkdir /nfsshare                         # creates a folder to be shared
+mount /dev/pcmkvg/pcmklv /nfsshare      # now mount volume to the shared folder
 
 ### test create a file
 mkdir -p /nfsshare/exports
@@ -98,23 +98,28 @@ mkdir -p /nfsshare/exports/export
 touch /nfsshare/exports/export/test.txt
 
 ### tag volume group to pacemaker
-vgchange --addtag pacemaker /dev/pcmkvg
-vgs -o vg_tags /dev/pcmkvg
+vgchange --addtag pacemaker /dev/pcmkvg # changes vg attrutes of /dev/pcmkvg (adds a tag)
+vgs -o vg_tags /dev/pcmkvg              # displays informationi about the volume group under the tag (pacemaker, in this case)
 
 ### unmount the volume and set activation flag
-umount /dev/pcmkvg/pcmklv
-vgchange -an pcmkvg
+umount /dev/pcmkvg/pcmklv               # vg is enabled at creation by default. this means, lv can be accessed in the vg. for clustering,
+vgchange -an pcmkvg                     # it needs to unmount and disable the volume group so that kernal does not know of it exists.
 
 ##########################################################################################
 ### run on each nodes !!!
 ##########################################################################################
+# The following procedure configures the volume group in a way that will ensure that only the cluster is capable of activating the volume 
+# group, and that the volume group will not be activated outside of the cluster on startup. If the volume group is activated by a system 
+# outside of the cluster, there is a risk of corrupting the volume group's metadata.
+
 lvmconf --enable-halvm --services --startstopservices
 
 ##########################################################################################
 ### run on node 1
 ##########################################################################################
 
-### add volume exclusion in lvm.conf file (you can use sed command)
+# add volume exclusion in lvm.conf file by adding volume_list = ["rootvg"] only. please note that you must NOT include your vg (pcmkvg) for 
+# it to be controlled by cluster
 vim /etc/lvm/lvm.conf # ---> LINE 1240: volume_list = ["rootvg"]
 
 ### rebuild initramfs once lvm.conf is modified
